86a18a80824bcc793a59f9dfdc1fb4c7bba92d9b
intellign <ismailarashad@aol.com>
2024-11-19 05:35:52 -0500

    Update 4 ffmpeg exporter.dart

--- lib/src/exporter.dart
+++ lib/src/exporter.dart
@@ -1,150 +1,100 @@
 import 'dart:ui' as ui show ImageByteFormat;
+import 'dart:io';
+import 'dart:async';
 
 import 'package:flutter/foundation.dart';
-import 'package:image/image.dart' as image;
-import 'frame.dart';
+import 'package:flutter/material.dart';
+import 'package:screen_recorder/src/constants.dart';
+import 'package:screen_recorder/src/render_type.dart';
 
-class Exporter {
-  final List<Frame> _frames = [];
-  int _maxWidthFrame = 0;
-  int _maxHeightFrame = 0;
+import 'package:ffmpeg_kit_flutter/ffmpeg_kit.dart';
 
-  List<Frame> get frames => _frames;
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
 
-  void onNewFrame(Frame frame) {
-    _frames.add(frame);
-  }
+class Exporter with ChangeNotifier {
+  bool loading = false, isPlaying = false;
 
-  void clear() {
-    _frames.clear();
+  Future<Map<String, dynamic>> mergeIntoVideo({
+    required RenderType renderType,
+  }) async {
+    loading = true;
+    notifyListeners();
 
-    _maxWidthFrame = 0;
-    _maxHeightFrame = 0;
-  }
+    /// get application temp directory
+    Directory appDocDirectory = await getTemporaryDirectory();
+    String dir = appDocDirectory.path;
 
-  bool get hasFrames => _frames.isNotEmpty;
+    basePath = "$dir/";
 
-  Future<List<RawFrame>?> exportFrames() async {
-    if (_frames.isEmpty) {
-      return null;
-    }
-    final bytesImages = <RawFrame>[];
-    for (final frame in _frames) {
-      final bytesImage =
-          await frame.image.toByteData(format: ui.ImageByteFormat.png);
+    ///////final FlutterFFmpeg _flutterFFmpeg = FlutterFFmpeg();
 
-      if (frame.image.width >= _maxWidthFrame) {
-        _maxWidthFrame = frame.image.width;
-      }
+    if (await Permission.storage.request().isGranted) {
+      /// mp4 output
+      String mp4Command =
+          '-r 50 -i ${Constants.imagesPath} -vf scale=1920:1080 -pix_fmt yuv420p -y ${Constants.videoOutputPath}';
 
-      if (frame.image.height >= _maxHeightFrame) {
-        _maxHeightFrame = frame.image.height;
-      }
+      /// 7mb gif output
+      String gifCommand =
+          '-r 50 -i ${Constants.imagesPath} -vf "scale=iw/2:ih/2" -y ${Constants.gifOutputPath}';
 
-      if (bytesImage != null) {
-        bytesImages.add(RawFrame(16, bytesImage));
-      } else {
-        debugPrint('Skipped frame while enconding');
-      }
-    }
-    return bytesImages;
-  }
+      /// Replacing audio stream
+      /// -c:v copy -c:a aac -map 0:v:0 -map 1:a:0
 
-  Future<List<int>?> exportGif() async {
-    final frames = await exportFrames();
-    if (frames == null) {
-      return null;
-    }
-    return compute(
-        _exportGif, DataHolder(frames, _maxWidthFrame, _maxHeightFrame));
-  }
+      /// To combine audio with video
+      //   String commandToExecute =
+      //     '-r 15 -f mp4 -i ${Constants.VIDEO_PATH} -f mp3 -i ${Constants.AUDIO_PATH} -c:v copy -c:a aac -map 0:v:0 -map 1:a:0 -t $timeLimit -y ${Constants.OUTPUT_PATH}';
 
-  static Future<List<int>?> _exportGif(DataHolder data) async {
-    List<RawFrame> frames = data.frames;
-    int width = data.width;
-    int height = data.height;
+      /// To combine audio with image
+      // String commandToExecute =
+      //     '-r 15 -f mp3 -i ${Constants.AUDIO_PATH} -f image2 -i ${Constants.IMAGE_PATH} -pix_fmt yuv420p -t $timeLimit -y ${Constants.OUTPUT_PATH}';
 
-    image.Image mainImage = image.Image.empty();
+      /// To combine audio with gif
+      // String commandToExecute = '-r 15 -f mp3 -i ${Constants
+      //     .AUDIO_PATH} -f gif -re -stream_loop 5 -i ${Constants.GIF_PATH} -y ${Constants
+      //     .OUTPUT_PATH}';
 
-    for (final frame in frames) {
-      final iAsBytes = frame.image.buffer.asUint8List();
-      final decodedImage = image.decodePng(iAsBytes);
+      /// To combine audio with sequence of images
+      // String commandToExecute = '-r 30 -pattern_type sequence -start_number 01 -f image2 -i ${Constants
+      //     .IMAGES_PATH} -f mp3 -i ${Constants.AUDIO_PATH} -y ${Constants
+      //     .OUTPUT_PATH}';
 
-      if (decodedImage == null) {
-        debugPrint('Skipped frame while enconding');
-        continue;
-      }
-      decodedImage.frameDuration = frame.durationInMillis;
-      mainImage.frames.add(
-        _encodeGifWIthTransparency(
-          image.copyExpandCanvas(
-            decodedImage,
-            newWidth: width,
-            newHeight: height,
-            toImage: image.Image(
-              width: width,
-              height: height,
-              format: decodedImage.format,
-              numChannels: 4,
-            ),
-          ),
-        ),
-      );
-    }
+      var response = await FFmpegKit.execute(
 
-    return image.encodeGif(mainImage);
-  }
+              ///_flutterFFmpeg   /// await FFmpegKit
+              renderType == RenderType.gif ? gifCommand : mp4Command)
+          .then((rc) async {
+        loading = false;
+        notifyListeners();
+        debugPrint(
+            'FFmpeg process exited with rc ==> ${await rc.getReturnCode()}');
+        debugPrint('FFmpeg process exited with rc ==> ${rc.getCommand()}');
+        var res = await rc.getReturnCode();
 
-  static image.PaletteUint8 _convertPalette(image.Palette palette) {
-    final newPalette = image.PaletteUint8(palette.numColors, 4);
-    for (var i = 0; i < palette.numColors; i++) {
-      newPalette.setRgba(
-          i, palette.getRed(i), palette.getGreen(i), palette.getBlue(i), 255);
-    }
-    return newPalette;
-  }
-
-  static image.Image _encodeGifWIthTransparency(image.Image srcImage,
-      {int transparencyThreshold = 1}) {
-    final newImage = image.quantize(srcImage);
-
-    // GifEncoder will use palette colors with a 0 alpha as transparent. Look at the pixels
-    // of the original image and set the alpha of the palette color to 0 if the pixel is below
-    // a transparency threshold.
-    final numFrames = srcImage.frames.length;
-    for (var frameIndex = 0; frameIndex < numFrames; frameIndex++) {
-      final srcFrame = srcImage.frames[frameIndex];
-      final newFrame = newImage.frames[frameIndex];
-
-      final palette = _convertPalette(newImage.palette!);
-
-      for (final srcPixel in srcFrame) {
-        if (srcPixel.a < transparencyThreshold) {
-          final newPixel = newFrame.getPixel(srcPixel.x, srcPixel.y);
-          palette.setAlpha(
-              newPixel.index.toInt(), 0); // Set the palette color alpha to 0
+        if (res!.getValue() == 0) {
+          return {
+            'success': true,
+            'msg': 'Widget was render successfully.',
+            'outPath': renderType == RenderType.gif
+                ? Constants.gifOutputPath
+                : Constants.videoOutputPath
+          };
+        } else if (res.getValue() == 1) {
+          return {'success': false, 'msg': 'Widget was render unsuccessfully.'};
+        } else {
+          return {'success': false, 'msg': 'Widget was render unsuccessfully.'};
         }
-      }
+      });
 
-      newFrame.data!.palette = palette;
+      return response;
+    } else if (await Permission.storage.isPermanentlyDenied) {
+      loading = false;
+      notifyListeners();
+      openAppSettings();
+      return {'success': false, 'msg': 'Missing storage permission.'};
+    } else {
+      return {'success': false, 'msg': 'unknown error.'};
     }
-
-    return newImage;
   }
-}
-
-class RawFrame {
-  RawFrame(this.durationInMillis, this.image);
-
-  final int durationInMillis;
-  final ByteData image;
-}
-
-class DataHolder {
-  DataHolder(this.frames, this.width, this.height);
-
-  List<RawFrame> frames;
-
-  int width;
-  int height;
 }
