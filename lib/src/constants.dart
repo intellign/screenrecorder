da221f2d1aa951ed7498ac6806a51c2ec6092e28
intellign <ismailarashad@aol.com>
2024-11-19 05:34:37 -0500

    Add files via upload

---
+++ lib/src/constants.dart
@@ -0,0 +1,7 @@
+String basePath = '/data/user/0/com.example.example2/cache/';
+
+class Constants {
+  static String imagesPath = basePath + '%0d.png';
+  static String gifOutputPath = basePath + 'output.gif';
+  static String videoOutputPath = basePath + 'output.mp4';
+}
