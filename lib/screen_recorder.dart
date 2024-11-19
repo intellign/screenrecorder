115f76442a761ccc511b7b0db9ee90ab5129c551
intellign <ismailarashad@aol.com>
2024-11-19 05:38:20 -0500

    Update screen_recorder.dart

--- lib/screen_recorder.dart
+++ lib/screen_recorder.dart
@@ -1,5 +1,6 @@
 library screenrecorder;
 
-export 'src/exporter.dart';
-export 'src/frame.dart';
 export 'src/screen_recorder.dart';
+export 'src/frame.dart';
+export 'src/exporter.dart';
+export 'src/render_type.dart';
